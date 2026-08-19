fa5acd83c69b094bcd7db2eb2981e5d15a8dc508
sandesh patil <sandeshpatil1396@gmail.com>
2026-08-19 08:44:36 +0530

    Fix CORS for Netlify and add Dockerfile

--- src/main/java/com/skillforge/config/CorsConfig.java
+++ src/main/java/com/skillforge/config/CorsConfig.java
@@ -18,7 +18,8 @@ public class CorsConfig {
                                 "http://localhost:5173",
                                 "http://localhost:5174",
                                 "http://127.0.0.1:5173",
-                                "http://127.0.0.1:5174"
+                                "http://127.0.0.1:5174",
+                                "https://skillforgecourse.netlify.app"
                         )
                         .allowedMethods(
                                 "GET",
